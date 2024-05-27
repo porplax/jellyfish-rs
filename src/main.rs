a685783fa6e03943f751f5afac6afbbd14e15c59
porplax <saynemarsh9@gmail.com>
2024-05-27 09:27:59 -0400

    Optimized algorithms, converted to CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+#![windows_subsystem = "windows"]
+
 use neobridge_rust::{ Neobridge, RGB };
-use toml::Table;
 use xcap::Monitor;
 use image::{ self, Pixel };
 use std::{ thread, time::Duration };
 use nalgebra::{ DMatrix, Dyn, OMatrix };
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author="cutesunshine", version, about="Ambient lighting on neopixel devices.")]
+struct Args {
+    #[arg(short, long)]
+    width: u32,
+    height: u32,
+    n_of_leds: u32,
+
+    #[arg(short, long, default_value_t = 132)]
+    depth: usize,
+
+    #[arg(short, long, default_value_t = 60)]
+    refresh_rate: u64
+}
 
 type DMatrixu16 = OMatrix<f32, Dyn, Dyn>;
 
@@ -14,89 +28,85 @@ struct JellyRenderer {
     neobridge: Neobridge,
     monitors: Vec<Monitor>,
 
+    width: u32, 
+    height: u32,
+    n_of_leds: u32,
+
     x: u32, 
     y: u32,
 
-    height_capture: usize,
+    depth: usize,
 }
 
 impl JellyRenderer {
-    pub fn new(neobridge: Neobridge, monitors: Vec<Monitor>, height_capture: usize) -> JellyRenderer {
+    pub fn new(neobridge: Neobridge, monitors: Vec<Monitor>, depth: usize, width: u32, height: u32, n_of_leds: u32) -> JellyRenderer {
         JellyRenderer {
             neobridge,
             monitors,
 
+            width,
+            height,
+            n_of_leds,
+
             x: 0,
             y: 0,
             
-            height_capture
+            depth
         }
     }
 
     pub fn initialize(&mut self) {
-        self.neobridge.set_all(RGB(0, 0, 0));
+        self.neobridge.set_all(RGB                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          (0, 0, 0));
         self.neobridge.show();
     }
 
     pub fn grab_colors(&mut self) -> Vec<RGB> {
-        let image = self.monitors.get(0).unwrap().capture_image().unwrap();
-
         let mut colors: Vec<RGB> = vec![];
         self.x = 0;
 
-        let mut channels: DMatrixu16 = DMatrix::identity(self.height_capture, 3);
-
-        for _x in 0..30 {
-            self.y = 1079;
-
-            for _y in 0..self.height_capture {
-                let rgb_val = image.get_pixel(self.x, self.y);
-                channels[(_y, 0)] = rgb_val.channels()[0] as f32; // R
-                channels[(_y, 1)] = rgb_val.channels()[1] as f32; // G
-                channels[(_y, 2)] = rgb_val.channels()[2] as f32; // B
-
-                self.y -= 1;
+        let mut channels: DMatrixu16 = DMatrix::identity(self.depth, 3);
+        if let Ok(image) = self.monitors.get(0).unwrap().capture_image() {
+            for _x in 0..self.n_of_leds {
+                self.y = self.height-1;
+    
+                for _y in 0..self.depth {
+                    let rgb_val = image.get_pixel(self.x, self.y);
+                    channels[(_y, 0)] = rgb_val.channels()[0] as f32; // R
+                    channels[(_y, 1)] = rgb_val.channels()[1] as f32; // G
+                    channels[(_y, 2)] = rgb_val.channels()[2] as f32; // B
+    
+                    self.y -= 1;
+                }
+    
+                let rgb_convert = RGB(
+                    channels.column(0).mean() as u8,
+                    channels.column(1).mean() as u8,
+                    channels.column(2).mean() as u8
+                );
+                
+                colors.push(rgb_convert);
+                self.x += self.width / self.n_of_leds;
             }
-
-            let rgb_convert = RGB(
-                channels.column(0).mean() as u8,
-                channels.column(1).mean() as u8,
-                channels.column(2).mean() as u8
-            );
-            
-            colors.push(rgb_convert);
-            self.x += 66;
-        }
+        } 
         colors
     }
-
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 2 {
-        panic!("expected file name, got {}", args.len());
-    }
-
-    let file = File::open(&args[1]);
-    let mut contents = String::new();
-    file.expect("file aint openin").read_to_string(&mut contents);
-
-    let toml = contents.parse::<Table>().unwrap();
-    let frame_rate = toml["loop"]["frame_rate"].as_integer().unwrap();
-    let height_capture = toml["jellyfish"]["height_capture"].as_integer().unwrap();
-
-    println!("Set frame rate to {}", frame_rate);
-    println!("Set height capture to {}", height_capture);
+    let args = Args::parse();
 
     let neobridge = Neobridge::new("COM3", 30);
     let monitors = Monitor::all().unwrap();
 
     let mut jelly = JellyRenderer::new(
-        neobridge, 
+        neobridge,
         monitors,
-        height_capture as usize);
+        args.depth,
+        args.width,
+        args.height,
+        args.n_of_leds
+        );
+
     jelly.initialize();
 
     loop {
@@ -104,6 +114,6 @@ fn main() {
 
         jelly.neobridge.set_list(colors);
         jelly.neobridge.show();
-        thread::sleep(Duration::from_millis(1000 / frame_rate as u64));
+        thread::sleep(Duration::from_millis(1000 / args.refresh_rate as u64));
     }
 }
