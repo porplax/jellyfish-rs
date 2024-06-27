46f751ec3bbd25e05ad2a78e6ba6ac32b319f474
porplax <saynemarsh9@gmail.com>
2024-06-27 07:53:38 -0400

    removed `ChannelStorage` dead code

--- src/channel_storage/mod.rs
+++ src/channel_storage/mod.rs
@@ -3,15 +3,7 @@ pub struct ChannelStorage {
 
     sum_of_r: u16,
     sum_of_g: u16,
-    sum_of_b: u16,
-
-    high_r: u8,
-    high_g: u8,
-    high_b: u8,
-
-    low_r: u8,
-    low_g: u8,
-    low_b: u8,
+    sum_of_b: u16
 }
 
 impl ChannelStorage {
@@ -21,15 +13,7 @@ impl ChannelStorage {
 
             sum_of_r: 0,
             sum_of_g: 0,
-            sum_of_b: 0,
-
-            high_r: 0,
-            high_g: 0,
-            high_b: 0,
-
-            low_r: 255,
-            low_g: 255,
-            low_b: 255,
+            sum_of_b: 0
         }
     }
 
@@ -37,27 +21,9 @@ impl ChannelStorage {
         self.sum_of_r = 0;
         self.sum_of_g = 0;
         self.sum_of_b = 0;
-
-        self.high_r = 0;
-        self.high_g = 0;
-        self.high_b = 0;
-
-        self.low_r = 255;
-        self.low_g = 255;
-        self.low_b = 255;
     }
 
     pub fn push(&mut self, r: u8, g: u8, b: u8) {
-        if r > self.high_r && g > self.high_g && b > self.high_b {
-            self.high_r = r;
-            self.high_g = g;
-            self.high_b = b;
-        } else if r < self.low_r && g < self.low_g && b < self.low_b {
-            self.low_r = r;
-            self.low_g = g;
-            self.low_b = b;
-        }
-
         self.sum_of_r = self.sum_of_r + (r as u16);
         self.sum_of_g = self.sum_of_g + (g as u16);
         self.sum_of_b = self.sum_of_b + (b as u16);
